f1ef9bf11c73e7ca593118159338693f46e8c6b2
Yowj <eugenecoderx@gmail.com>
2026-01-19 02:47:45 +0800

    Fixed UI of forms

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/ui/auth/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
         <LoginForm />
       </div>
     </div>
